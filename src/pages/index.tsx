6eb6c93a12bfcb3ba8a7b6cd3f689b0ec9e20a0a
koch21 <hagnakoch@gmail.com>
2021-08-29 18:57:48 -0300

    setup inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Koch21</title>
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </div>
+  )
+}
+export default Home
